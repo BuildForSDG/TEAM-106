4ea77e63a0259fa60ce59b1b8ce31b4c90a0c40b
Tunde4real <ib4real914@gmail.com>
2020-06-19 06:43:19 +0100

    remove all irrelevances and edited the app to present mvp

--- devdatazone/settings.py
+++ devdatazone/settings.py
@@ -84,23 +84,10 @@ WSGI_APPLICATION = 'devdatazone.wsgi.application'
 # }
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'tqfiezjk',
-
-        'USER': 'tqfiezjk',
-
-        'PASSWORD': 'NQpiZmAu6way358WmmXOr2NgyQhA6iT5',
-
-        'HOST': 'rogue.db.elephantsql.com',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 # Password validation
